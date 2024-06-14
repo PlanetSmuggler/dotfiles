13067e4b58d4398cbc2ff1f331541ff4279875ce
PlanetSmuggler <thotakuraharish14@gmail.com>
2024-06-14 15:44:40 +0530

    Save for Now

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -64,42 +64,130 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    
-  -- colorscheme
+ 
+  -- colorshceme
   {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    config = function() vim.cmd[[colorscheme tokyonight]] end
+    config = function() vim.cmd[[colorscheme tokyonight-night]] end
   },
 
-  -- TODO: file explorer
+  -- TODO: cutomize this status bar 
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {"nvim-tree/nvim-web-devicons"},
+    config = function()
+      require("lualine").setup({})
+    end
+  },
+
+  -- tabline
+  {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = {"nvim-tree/nvim-web-devicons"},
+    config = function()
+      require("bufferline").setup({})
+    end
+  },
+
+  -- explorer
   {
     "nvim-tree/nvim-tree.lua",
-    dependencies = "nvim-tree/nvim-web-devicons",
+    dependencies = {"nvim-tree/nvim-web-devicons"},
     config = function()
-      require("nvim-tree").setup({
-        auto_reload_on_write = true,
-        hijack_netrw = true,
-        view = {
-          width = 30,
-        },
-        renderer = {
-          indent_width = 1
-        },
-
-
-        -- TODO: root_dirs
-
-		
-      })
-      map("n", "<leader>et", "<cmd>NvimTreeToggle<CR>", opts)
-      map("n", "<leader>ef", "<cmd>NvimTreeFocus<CR>", opts)
-      map("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", opts)
-      map("n", "<leader>eh", "<cmd>lua require('nvim-tree.api').tree.toggle_help()<CR>")
+      vim.g.loaded_netrw = 1
+      vim.g.loaded_netrwPlugin = 1
+      require("nvim-tree").setup({})
     end
   },
 
 
+  -- auto pairs
+ {
+  "windwp/nvim-autopairs",
+  event = {"InsertEnter"},
+  dependencies = {"hrsh7th/nvim-cmp"},
+  config = function()
+    require("nvim-autopairs").setup({})
+  end
+ },
+
+ -- lsp
+ {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim"
+    },
+    config = function()
+      local mason = require("mason")
+      local mason_lspconfig = require("mason-lspconfig")
+      local lspconfig = require("lspconfig")
+
+      mason.setup({})
+      mason_lspconfig.setup({
+        ensure_installed = {"lua_ls","pyright"},
+        automatic_installation = true
+      })
+      lspconfig.lua_ls.setup({})
+    end
+ },
+
+ -- completions -- TODO pendina
+  {
+    "hrsh7th/nvim-cmp",
+    event = {"InsertEnter"},
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-emoji",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      {
+        "L3MON4D3/LuaSnip",
+        version = "v2.*",
+        build = "make install_jsregexp"
+      },
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+      "onsails/lspkind.nvim",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      local lspkind = require("lspkind")
+
+      require("luasnip.loaders.from_vscode").lazy_load()
+
+      cmp.setup({
+        completion = {
+          completeopt = "menu,menuone,preview,noselect"
+        },
+        snippet = {
+          expand = function(args) luasnip.lsp_expand(args.body)end
+        },
+        mapping = cmp.mapping.preset.insert({
+           ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+        ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+        ["<C-e>"] = cmp.mapping.abort(), -- close completion window
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        }),
+        sources = cmp.config.sources({
+        { name = "nvim_lsp"},
+        { name = "luasnip" }, -- snippets
+        { name = "buffer" }, -- text within current buffer
+        { name = "path" }, -- file system paths
+      }),
+      })
+
+    end
+
+  }
 
 })
+  
